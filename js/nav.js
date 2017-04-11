5817fac645bbac78e9356148cfe36e4ef27f37f0
Rebecca Wilkerson <nashvillesoftwareschool@Nashvilles-MacBook-Pro.local>
2017-04-11 10:54:53 -0500

    this should remove unecessary files from branch

--- js/nav.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-function displayNavIcon(navIcon) {
-  var icon = document.createElement("icon");
-  icon.src = "https://cdn3.iconfinder.com/data/icons/education/512/literature-512.png"+navIcon;
-  document.getElementById('navIcon').appendChild(icon); 
-};
